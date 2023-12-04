6654fe7def440b4cc21366790159970879b01c57
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2023-12-04 21:17:36 +0400

    The number of comparisons and copies has been calculated for arrays of length 1000, 2000, ..., 100000. Quick sort has been fixed.

--- src/main.cc
+++ src/main.cc
@@ -7,7 +7,7 @@ int main(){
     const std::vector<size_t> array_sizes = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
     random_experiment(array_sizes, 3);
     for (auto size : array_sizes) {
-        //sorted_array(size, 2);
-        //reverse_sorted_array(size, 3);
+        sorted_array(size, 2);
+        reverse_sorted_array(size, 3);
     }
 }
